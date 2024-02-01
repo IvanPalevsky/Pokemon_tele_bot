bda57c1ab05ff1eb63308d274bf2db4c2b4fd8ef
IvanPalevsky <132829974+IvanPalevsky@users.noreply.github.com>
2024-02-01 13:04:20 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -11,12 +11,10 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.hp = randint(10, 100)
+        self.power = randint(10, 30)
 
         Pokemon.pokemons[pokemon_trainer] = self
-
-    # Метод для получения картинки покемона через API
-    def get_img(self):
-        pass
     
     # Метод для получения картинки покемона через API
     def get_img(self):
@@ -38,26 +36,40 @@ class Pokemon:
         else:
             return "Pikachu"
 
-    def get_hp(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            return (data['sprites']["stats"]["base_stat"])
-        else:
-            return "Pikachu"
-    
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
-
-    # Метод класса для получения способности покемона
-    def info2(self):
-        return f"Здоровье твоего покеомона: {self.hp}"
+        return f"""Имя твоего покеомона: {self.name}
+сила покемона {self.power}
+здоровье покемона {self.hp}
+"""
     
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = randint(1,5)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+
+
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\n Боец применил супер-атаку силой:{super_power} "
+
+class Wizard(Pokemon):
+    pass
 
 
 
